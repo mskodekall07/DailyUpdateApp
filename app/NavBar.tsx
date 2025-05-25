32590089d19c89cded8ddbfda1f3168fb9021266
Mahananda Herundi <mskodekall07@gmail.com>
2025-05-25 06:30:40 -0700

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { IoBug } from "react-icons/io5";
+import classnames from "classnames";
 
 const NavBar = () => {
+    const currentPath=usePathname();
+   
     const links=[
         {label:"Dashboard", href:"/"},
         {label:"Issues", href:"/issues"},
@@ -12,7 +17,11 @@ const NavBar = () => {
         <Link href="/"><IoBug/></Link>
         <ul className="flex space-x-6">
         {links.map(link=><Link key={link.href} 
-        className="text-zinc-500 hover:text-zinc-800 transition-colors" 
+        className={classnames({
+          "text-zinc-900":link.href===currentPath,
+          "text-zinc-500":link.href!==currentPath,
+          "hover:text-zinc-800 transition-colors":true,
+        })}
         href={link.href}>
         {link.label}</Link>)}
         </ul>
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newIssuesPage = () => {
+  return (
+    <div>newIssuesPage</div>
+  )
+}
+
+export default newIssuesPage
